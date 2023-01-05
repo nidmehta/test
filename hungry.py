a7aa66f00cee19a05889ae1b36f9e61320544bf7
Nidhi Mehta <nidhi882000mehta@gmail.com>
2023-01-05 19:40:54 +0530

    commit to thirsty branch

--- hungry.py
+++ hungry.py
@@ -1,7 +1,10 @@
 hungry = input("are you hungry?")
 if hungry == "yes":
     print("eat samosa")
-    print("drink fanta")
+    print("eat shit")
     print("cry")
 else:
+    thirsty = input("are you thirsty?")
+    if thirsty == "yes":
+        print("drink water")
     print("sleep")
